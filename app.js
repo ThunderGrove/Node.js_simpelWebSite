eab693d2d00daf1890cbddf53c823c288d595fa0
ThunderGrove <rene699f@edu.easj.dk>
2020-09-09 12:28:02 +0200

    update app.ja and added menuGenerator and style.

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http=require('http');
+var url=require('url');
+var fs=require('fs');
+var page="index";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//create a server object:
+http.createServer(function(req,res){
+  res.writeHead(200,{'Content-Type':'text/html'});
+  var pageReq=url.parse(req.url,true).pathname;//Needed to get page accessed
+  res.write('<!DOCTYPE html><html><head><title>This is just a simple website made with Node.js</title><style>');
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  var style=fs.readFileSync('style/default.css','utf8');
+  res.write(style.toString());
+
+  res.write('</style></head><body><div id="pageContainer"><div id=pageHead>This is just a simple website made with Node.js</div><div id="menu">');
+  if(pageReq.localeCompare("/emner")==0){
+    page="emner";
+  }else if(pageReq.localeCompare("/praktikvirksomheder")==0){
+    page="praktikvirksomheder";
+  }
+
+  var menu=require('./includes/menu.js');
+  res.write(menu.menuGenerator(page));
+
+  res.write('</div><div id="page"></div><div></body></html>');
+  res.end();//end the response
+}).listen(8080); //the server object listens on port 8080
---
+++ includes/menu.js
@@ -0,0 +1,26 @@
+function menuGenerator(pageName){
+  var menuHTML="<ul>";
+
+  if(pageName=="index"){
+    menuHTML+='<li id="activePage"><a href="/">Forside</a></li>';
+  }else{
+    menuHTML+='<li><a href="/">Forside</a></li>';
+  }
+
+  if(pageName=="emner"){
+    menuHTML+='<li id="activePage"><a href="/emner">Emner</a></li>';
+  }else{
+    menuHTML+='<li><a href="/emner">Emner</a></li>';
+  }
+
+  if(pageName=="praktikvirksomheder"){
+    menuHTML+='<li id="activePage" class="last"><a href="/praktikvirksomheder">Praktikvirksomheder</a></li>';
+  }else{
+    menuHTML+='<li class="last"><a href="/praktikvirksomheder">Praktikvirksomheder</a></li>';
+  }
+
+  menuHTML+='<ul>';
+  return menuHTML;
+}
+
+module.exports.menuGenerator=menuGenerator;
